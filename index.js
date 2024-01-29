5ff8c54df8f1d2f84128572b5756c16b220bb2a2
Jeff Free <jefffreelance3@gmail.com>
2024-01-28 22:31:07 -0300

    highlights

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 let wheel = document.querySelector('.wheel');
 let spinBtn = document.querySelector('.spinBtn');
-let value = Math.ceil(Math.random() * 3600);
+let segments = document.querySelectorAll('.number');
+let resultText = document.querySelector('#resultText');
+let numberOfSegments = segments.length;
+let degreesPerSegment = 360 / numberOfSegments;
 
-spinBtn.onclick = function(){
-	wheel.style.transform = 'rotate(' + value + 'deg)';
-	console.log(value)
-	value += Math.ceil(Math.random() * 3600);
-}
+spinBtn.onclick = function () {
+	segments.forEach((segment, index) => {
+		segment.classList.remove('highlight');
+	})
+
+	let spins = Math.floor(Math.random() * 10 + 10);
+	let degrees = spins * 360;
+	let extraDegrees = Math.floor(Math.random() * 360);
+	let totalDegrees = degrees + extraDegrees;
+
+
+	wheel.style.transform = 'rotate(' + totalDegrees + 'deg)';
+
+
+	setTimeout(() => {
+
+		let winningSegment = Math.floor(((totalDegrees % 360) / degreesPerSegment) + 0.5) % numberOfSegments;
+		winningSegment = (numberOfSegments - winningSegment) % numberOfSegments;
+
+
+		segments.forEach((segment, index) => {
+			if (index === winningSegment) {
+				segment.classList.add('highlight');
+
+				let winningText = segment.textContent || segment.innerText;
+                resultText.innerText = `La forma elegida es ${winningText}!`;
+
+			}
+		});
+	}, 3000);
+};
